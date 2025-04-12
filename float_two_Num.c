8f47c21768e8af299ad5464027a573b5c170634a
Aname Alahi <anamealahi07@gmail.com>
2025-04-12 17:58:19 +0600

    Create float_two_Num.c
    1. C Program to Multiply two Floating-Point Numbers

---
+++ float_two_Num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float num1,num2,product;//variable declaration
+    printf ("Enter two floating-point number:");
+    scanf("%f%f",&num1,&num2);
+    product=num1*num2;
+    printf("Product%f\n",product);
+    return 0;
+    
+}
